ef0df3bdac3bd518f8410dca8487b279139a21ab
Raghuraman G <ragu123@aol.in>
2025-12-06 13:21:39 +0530

    Add files via upload
    Week_1_Day_01_JS_PracticeCode

---
+++ dataType.js
@@ -0,0 +1,8 @@
+var browserVersion = 10.0;
+console.log("The type of browserVersion is: " + typeof browserVersion);
+var browserName = "Google Chrome";
+console.log("The type of browserName is: " + typeof browserName);
+var isBrowserOpen = true;
+console.log("The type of isBrowserOpen is: " + typeof isBrowserOpen);
+var history= null;
+console.log("The type of history is: " + typeof history);
