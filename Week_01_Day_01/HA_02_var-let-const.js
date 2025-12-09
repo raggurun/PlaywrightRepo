42a8ec95b97bd8f614be12e08b7e4083549121de
Raghuraman G <ragu123@aol.in>
2025-12-09 11:56:04 +0530

    Refactor variable scope examples using var, let, const

--- Week_01_Day_01/HA_02_var-let-const.js
+++ Week_01_Day_01/HA_02_var-let-const.js
@@ -5,13 +5,13 @@
 
     // Function
     function getBrowserVersion() {
-    // Check condition
+    // Check condition -> undefined === chrome -> failed. so, if condition won't execute
       if (browserVersion === "Chrome") {
     // Local variable using 'var' (block declared but function-scoped)
-        var browserVersion = "Chrome 130"; // shadows the global within the function
+        var browserVersion = "Chrome 130";
         console.log("[inside if] browserVersion = " + browserVersion);
       }
-      // Prints the 'var' from the function scope (available outside the block)
+      // Prints "undefined"
       console.log("[outside block] browserVersion = " + browserVersion);
     }
 
@@ -22,7 +22,7 @@
     console.log("[global] browserVersion = " + browserVersion);
 
 
-	/* //Using let:
+	 //Using let:
 	
 	// Global variable
     const browserVersion = "Chrome";
@@ -43,4 +43,29 @@
     getBrowserVersion();
 
     // Show the global is unaffected
-    console.log("[global] browserVersion = " + browserVersion); */
+
+    console.log("[global] browserVersion = " + browserVersion); 
+
+	 //Using const:
+	
+	// Global variable
+    const browserVersion = "Chrome";
+
+    // Function
+    function getBrowserVersion() {
+    // Check condition
+      if (browserVersion === "Chrome") {
+        // 5) Local variable using 'let' (block-scoped)
+        const browserVersion = "Chrome 130"; // shadows only within the 'if' block
+        console.log("[inside if] browserVersion = " + browserVersion);
+      }
+    // Outside the 'if' block, the 'const' variable is out of scope; we see the global
+      console.log("[outside block] browserVersion = " + browserVersion);
+    }
+
+    // Call function
+    getBrowserVersion();
+
+    // Show the global is unaffected
+
+    console.log("[global] browserVersion = " + browserVersion); 
