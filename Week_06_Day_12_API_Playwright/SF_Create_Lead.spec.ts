81958fc46c743e94882f5a4579b443f0fec6b139
Raghuraman G <ragu123@aol.in>
2026-01-11 13:28:25 +0530

    Refactor Salesforce API tests for lead management

--- Week_06_Day_12_API_Playwright/SF_Create_Lead.spec.ts
+++ Week_06_Day_12_API_Playwright/SF_Create_Lead.spec.ts
@@ -15,9 +15,9 @@ test.describe.serial('Practice Salesforce API requests', () => {
       form: {
         "grant_type": "password",
         "username": "manikandanleo4922@agentforce.com",
-        "password": "India@2026",
-        "client_id": "3MVG9dAEux2v1sLs_5LgrWbWWJbMYKRgBajBibwGyik0pC_tXNFGsk6aV8h1owvGj6hsaxRWhzGX5WP1O87h5",
-        "client_secret": "EE7E666EF8204C869125AC8160C78B0F93277C544B84F8B89A0D59AD9FB3AD1C"
+        "password": "***",
+        "client_id": "***",
+        "client_secret": "***"
       }
     });
 
@@ -119,3 +119,4 @@ test.describe.serial('Practice Salesforce API requests', () => {
       console.log("Lead deleted successfully");     
   });
 });
+
