4b6f4894aab3830b190b2c4b6635507a17a90339
Raghuraman G <ragu123@aol.in>
2026-01-18 12:02:43 +0530

    Update click selector in LT_Testcase.spec.ts

--- Week_07_Day_14/CustomFixture/LT_Testcase.spec.ts
+++ Week_07_Day_14/CustomFixture/LT_Testcase.spec.ts
@@ -4,7 +4,5 @@ customFixture("SalesForce Application Sanity Testing",async({Leaftaps})=>{
 
 const title= await Leaftaps.title()
 console.log("Title of the page:",title)
-
-await Leaftaps.click('img[src="/opentaps_images/integratingweb/crm.png"]')
-
+await Leaftaps.click('text=CRM/SFA')
 })
